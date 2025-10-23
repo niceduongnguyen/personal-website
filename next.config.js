86cffa4a644d0d8c67ade3204c08bcdbcb18fc7d
niceduongnguyen <194105508+niceduongnguyen@users.noreply.github.com>
2025-10-23 18:51:38 -0500

    chore: prettify configs

--- next.config.js
+++ next.config.js
@@ -1,29 +1,20 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/usaco-classes',
-        destination: 'https://classes.niceduongnguyen.me/',
-        permanent: false,
-      }
-    ]
-  },
-  async rewrites() {
-    return {
-      // We need this rewrite in beforeFiles, otherwise
-      // tcw.sh will still serve the normal homepage
-      beforeFiles: [
-        {
-          source: '/',
-          has: [
-            {
-              type: 'host',
-              value: 'tcw.sh',
-            },
-          ],
-          destination: '/l/',
-        },
-      ]
-    }
-  },
-}
+	async rewrites() {
+		return {
+			// We need this rewrite in beforeFiles, otherwise
+			// tcw.sh will still serve the normal homepage
+			beforeFiles: [
+				{
+					source: '/',
+					has: [
+						{
+							type: 'host',
+							value: 'tcw.sh'
+						}
+					],
+					destination: '/l/'
+				}
+			]
+		};
+	}
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['app/**/*.{js,ts,tsx}','components/**/*.{js,ts,tsx}','utils/**/*.{js,ts,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        homepageIcon: "#225da5",
-        accent: colors.fuchsia["400"]
-      },
-      zIndex: {
-        '-10': '-10',
-      }
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+	content: ['app/**/*.{js,ts,tsx}', 'components/**/*.{js,ts,tsx}', 'utils/**/*.{js,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				homepageIcon: '#225da5',
+				accent: colors.fuchsia['400']
+			},
+			zIndex: {
+				'-10': '-10'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/forms')]
+};
